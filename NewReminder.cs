cee2931823a257d81ce32d78d53ee6cd36376281
Marino <marinoj.business@gmail.com>
2023-07-05 14:11:24 +0200

    design changes

--- NewReminder.cs
+++ NewReminder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Universal_Launcher
 {
-    public partial class NewReminder : Form
+    public partial class NewReminder : MetroForm
     {
        public Reminders Reminders;
        public Reminder Reminder { get; set; }
